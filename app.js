e982fcaaa80a3bf18a0988054bc59be87dac6bc8
vignesh <vignesh.srinivasan@pearson.com>
2016-05-22 10:12:59 +0530

    app

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
 var fs = require("fs"),
-	http = require("http"),
-	path = require("path");
+    http = require("http"),
+    path = require("path");
 
 
-http.createServer(function(request,response){
-	var fileName = "index.html",
-		contentType = "text/html";
+http.createServer(function(request, response) {
+    var fileName = "index.html",
+        contentType = "text/html";
 
-	if( request.url !== "/"){
-		var extName = path.extname(request.url);
-		fileName = request.url.replace("/","");
-		switch (extName) {
-		    case '.js':
-		        contentType = 'text/javascript';
-		        break;
-		    case '.html':
-		        contentType = 'text/html';
-		        break;
-		    case '.css':
-		        contentType = 'text/css';
-		        break;
-		    case '.json':
-		        contentType = 'application/json';
-		        break;
-		    case '.png':
-		        contentType = 'image/png';
-		        break;      
-		    case '.jpg':
-		        contentType = 'image/jpg';
-		        break;
-		    case '.wav':
-		        contentType = 'audio/wav';
-		        break;
-		}
-	}
+    if (request.url !== "/") {
+        var extName = path.extname(request.url);
+        fileName = request.url.replace("/", "");
+        switch (extName) {
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.html':
+                contentType = 'text/html';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.json':
+                contentType = 'application/json';
+                break;
+            case '.png':
+                contentType = 'image/png';
+                break;
+            case '.jpg':
+                contentType = 'image/jpg';
+                break;
+            case '.wav':
+                contentType = 'audio/wav';
+                break;
+        }
+    }
 
-	fs.readFile(fileName,function(err,data){
-		response.writeHead("200",{"content-type":contentType});
-		response.end(data);
-	});
+    fs.readFile(fileName, function(err, data) {
+        response.writeHead("200", {
+            "content-type": contentType
+        });
+        response.end(data);
+    });
 }).listen(8001);
 
-console.log("Server Started 8001");
+console.log("Server Started 80012");
